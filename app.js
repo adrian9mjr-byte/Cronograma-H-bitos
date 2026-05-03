9f5c827ba4b7074bc9565283685f26b90c0fd151
adrian9mjr-byte <adrian9mjr@gmail.com>
2026-05-03 00:01:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -515,4 +515,4 @@ function App(){
         )
       )
     }
-ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(App));
+    ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(App));
